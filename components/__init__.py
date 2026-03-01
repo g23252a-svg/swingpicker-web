e7c7aa0bce8a7e58f9f42c4cc9e89212ae7340a1
g23252a-svg <g23252a@gmail.com>
2026-03-01 12:41:44 +0900

    hotfix: components/__init__.py 자동 import 제거 (순환참조 수정)

--- components/__init__.py
+++ components/__init__.py
@@ -1,5 +1,2 @@
 # components package
-from .tab_market import render_tab1_market
-from .tab_stocks import render_tab2_stocks
-
-__all__ = ["render_tab1_market", "render_tab2_stocks"]
+# 개별 탭은 필요할 때 직접 import (자동 import 시 순환참조 위험)
